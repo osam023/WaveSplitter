51c038dc0030f20dcbfb23e6d0c22102d889ddd6
osam023 <sh5150@gmail.com>
2017-03-14 13:40:49 +0900

    cleanup

--- WaveSplit.py
+++ WaveSplit.py
@@ -2,29 +2,25 @@
 # -*- coding: utf-8 -*-
 
 import wave
-import sys
 import os
 import click
 
+OUTPUT_FILE_NAME = 'output'
+COMBINE_CHANNEL = 2
+COMBINE_SAMPLE = 2
+COMBINE_FRAMERATE = 44100
+
 """
- this class is edit wave file divide/combile.
+ This class is edit wave file divide/combine.
  
  TODO: support logfile.
 """
 class WaveSplitter(object):
-    __wave_data = None
     __wave_path = ''
-
-    __filesize = 0
-    __onefilesize = 0
     __split_size = 1
 
-    __combine_channel = 2
-    __combine_sample = 2
-    __combine_framerate = 44100
-
-    __output_name = "output"
-    __workdir = ""
+    __output_dir = ''
+    __workdir = ''
 
     __mode = 0 # 0:divid, 1:combine
 
@@ -35,8 +31,7 @@ class WaveSplitter(object):
     ## wave file open.
     def set_wavefile(self, filename):
         self.__wave_path = os.path.dirname(os.path.abspath(filename))
-        self.__wave_data = wave.open(filename, 'rb')
-        self.__filesize = self.__wave_data.getnframes()
+        self.__wave_filename = filename
             
     ## split file size.
     def set_splitsize(self, num):
@@ -47,55 +42,61 @@ class WaveSplitter(object):
 
     ## divide wave file.
     def _divide(self):
-        split_file_size = self.__filesize / self.__split_size
-        
-        pos = [0]
+        filename = os.path.join(self.__wave_path, self.__wave_filename)
+        wave_data =  wave.open(filename, 'rb')
+        filesize = wave_data.getnframes()
+        split_file_size = filesize / self.__split_size
+
+        position = [0]
         tmp = 0
-        for x in range(0, self.__split_size):
+        for item in range(0, self.__split_size):
             tmp += split_file_size
-            if self.__filesize > tmp:
-                if tmp < self.__filesize - 10240:
-                    pos.append(int(tmp))
+            if filesize > tmp:
+                if tmp < filesize - 10240:
+                    position.append(int(tmp))
 
-        channels = self.__wave_data.getnchannels()
-        sample = self.__wave_data.getsampwidth()
-        framerate = self.__wave_data.getframerate()
+        channels = wave_data.getnchannels()
+        sample = wave_data.getsampwidth()
+        framerate = wave_data.getframerate()
 
-        cnt = 0
+        counter = 0
         start = 0
         end = 0
-        for x in pos:
-            cnt += 1
-            start = x
+        for item in position:
+            counter += 1
+            start = item
             try:
-                if cnt < self.__split_size:
-                    end = self.__filesize / self.__split_size
+                if counter < self.__split_size:
+                    end = filesize / self.__split_size
                 else:
-                    end = self.__filesize - ((self.__filesize / self.__split_size) * (self.__split_size - 1))
+                    end = filesize - ((filesize / self.__split_size) * (self.__split_size - 1))
             except IndexError, ie:
                 pass
-                #print ie
-            self._output_file(start, end, cnt, channels, sample, framerate)
-
+            self._output_file(start, end, counter, channels, sample, framerate, wave_data)
+            
     ## write wave file.
-    def _output_file(self, start, end, count, channels, sample, framerate):
+    def _output_file(self, start, end, count, channels, sample, framerate, wave_data):
         self._check_workdir()
 
-        filename = self.__workdir + self._prefix(count) + "_" + self.__output_name + ".wav"
+        output_filename = self._prefix(count) + '_' + OUTPUT_FILE_NAME + '.wav'
+        filename = os.path.join(self.__workdir, output_filename)
 
         output_data = wave.open(filename, 'wb')
         output_data.setnchannels(channels)
         output_data.setsampwidth(sample)
         output_data.setframerate(framerate)
     
-        self.__wave_data.setpos(start)
-        output_data.writeframes(self.__wave_data.readframes(end))
+        wave_data.setpos(start)
+        output_data.writeframes(wave_data.readframes(end))
         output_data.close()
+
+    def set_outputdir(self, output_dir):
+        self.__output_dir = output_dir
 
     ## checking work directory.
     ## if work directory not exists, create work directory.
     def _check_workdir(self):
-        self.__workdir = self.__wave_path + "/out/"
+        self.__workdir = os.path.join(self.__wave_path, self.__output_dir)
         if not os.path.exists(self.__workdir):
             os.mkdir(self.__workdir)
 
@@ -111,12 +112,12 @@ class WaveSplitter(object):
 
     ## combine wave files.
     def _combine(self):
-        if self.__output_name == "output":
-            self.__output_name == "combine.wav"
+        if self.__output_name == 'output':
+            self.__output_name == 'combine.wav'
         
         divide_files = []
         for filename in os.listdir(self.__workdir):
-            filename = self.__workdir + filename
+            filename = os.path.join(self.__workdir, filename)
             divide_files.append(wave.open(filename, 'rb'))
 
         output_data = wave.open(self.__output_name, 'wb')
@@ -142,11 +143,12 @@ class WaveSplitter(object):
 @click.command(help='Wave file splitter')
 @click.option('-s', '--split-size', 'split_size', type=int, help='split size', default=2, required=False)
 @click.option('-i', '--input-file', 'input_file', type=str, help='input file path(wave file only)', required=True)
-@click.option('-o', '--output-dir', 'output_dir', type=str, help='output directory path', default='.', required=False)
+@click.option('-o', '--output-dir', 'output_dir', type=str, help='output directory path', default='out', required=False)
 def main(split_size, input_file, output_dir):
     ws = WaveSplitter()
     ws.set_splitsize(split_size)
     ws.set_wavefile(input_file)
+    ws.set_outputdir(output_dir)
     ws.run()
 
 if __name__ == '__main__':
